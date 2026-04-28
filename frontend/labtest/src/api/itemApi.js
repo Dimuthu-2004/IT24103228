9b260eca1757f3c1cf6b210bc8210bc35d4f0231
Fonseka N.H.D.K IT24103228 <it24103228@my.sliit.lk>
2026-04-28 10:18:23 +0530

    fix api url for deployment

--- frontend/labtest/src/api/itemApi.js
+++ frontend/labtest/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api/items",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getItems = () => API.get("/");
